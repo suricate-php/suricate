90be47b003932e4d091939a7fe5df9e78611c865
Mathieu LESNIAK <mathieu@lesniak.fr>
2014-02-28 22:59:46 +0100

    [Upg] App boot

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Suricate - App entry point
+ *
+ * @package  Suricate
+ * @author   Mathieu LESNIAK <mathieu@lesniak.fr>
+ */
+
+// Use composer autoloader
+require '../vendor/autoload.php';
+
+// or use Suricate autoloader
+
+$app = new Suricate\Suricate('../app/config/app.ini');
+$app->run();
