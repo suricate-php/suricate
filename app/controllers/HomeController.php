002709303f245b5096827b6f64ef8ec1f0cfa862
Mathieu LESNIAK <mathieu@lesniak.fr>
2014-02-28 22:59:29 +0100

    [Fix] right view

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,12 +1,13 @@
 <?php
 namespace App\Controllers;
 
+use App\Views;
 
 class HomeController extends BaseController
 {
     public function getHomepage()
     {
-        $view = new View\Weather\Country($model);
+        $view = new Views\BaseView();
         $this->response->setBody($view->render());
     }
 }
