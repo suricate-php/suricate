d32dac9d5e614143d430f5f89d9c5150c5d7c9e4
Mathieu LESNIAK <maverick@eskuel.net>
2014-08-03 15:57:32 +0200

    [Upg] Path + helpers base file

--- app/bootstrap/start.php
+++ app/bootstrap/start.php
@@ -1,14 +1,22 @@
 <?php
 /**
- * Suricate - App entry point
+ * Suricate - App starter
+ * Setup everything and return an $app Suricate instance
  *
  * @package  Suricate
  * @author   Mathieu LESNIAK <mathieu@lesniak.fr>
  */
 
 // Use composer autoloader
-require '../../vendor/autoload.php';
+require '../vendor/autoload.php';
 //ini_set('display_errors', 'On');
 // or use Suricate autoloader
 
-$app = new Suricate\Suricate('../app/config/app.ini');
+$app = new Suricate\Suricate(
+	require 'paths.php',
+	'../app/config/app.ini'
+);
+
+if (is_readable('../helpers.php')) {
+	require '../helpers.php';
+}
---
+++ app/helpers.php
@@ -0,0 +1,4 @@
+<?php
+/**
+Here goes helpers
+**/
