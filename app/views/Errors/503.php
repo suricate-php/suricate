6c655094c1441897945f30eff77b50f4cda4c86e
Mathieu LESNIAK <maverick@eskuel.net>
2015-02-16 22:40:17 +0100

    [New]Maintenance default page

---
+++ app/views/Errors/503.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Maintenance in progress</title>
+        <style>
+            body{font-family: "Open Sans",arial,sans-serif; background: #FFF; color:#333; margin:2em}
+            code{background:#E0941B;border-radius:4px;padding:2px 6px}
+        </style>
+    </head>
+    <body>
+        <h1>Maintenance in progress</h1>
+        <h2>Please be patient, we'll be back soon !</h2>
+    </body>
+</html>
